5e15d5e3a38f2def322e19c668a837eece670468
Arclight-V <vladimtor@gmail.com>
2021-08-27 16:21:09 +0300

    Add a car mileage reading
    This parameter is used to calculate the price forecast.

--- predict/predict.cpp
+++ predict/predict.cpp
@@ -3,18 +3,32 @@
 
 
 int main() {
-    double          theta0 = 0.0, theta1 = 0.0;
+    double          theta0 = 0.0, theta1 = 0.0, millage = 0.0;
     std::string     line;
     std::ifstream   in("../../coefficient.txt");
     if (in.is_open()) {
         std::getline(in,line);
         theta0 = std::stod(line);
         theta1 = std::stod(line.substr(line.find(' '), line.size()));
-        std::cout << theta0 << " " << theta1 << '\n';
     } else {
         std::cout << "File opening error\n";
         return -1;
     }
+
+    std::cout << "Enter the positive mileage of the car: \n";
+    std::getline(std::cin, line);
+
+    try {
+        millage = std::stod(line);
+    } catch (std::invalid_argument) {
+        std::cout << "No conversion could be performed\n";
+        return -1;
+    } catch (std::out_of_range) {
+        std::cout << "The converted value would fall out of the range of the result type or if the underlying function\n";
+        return -1;
+    }
+
+    std::cout << theta0 << " " << theta1 << ' ' << millage << '\n';
 }
 
 //
