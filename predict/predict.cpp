f65be5d216e94f2a294ce6c2359d487a09fcc8de
Arclight-V <vladimtor@gmail.com>
2021-08-27 16:43:39 +0300

    Add dataset

--- predict/predict.cpp
+++ predict/predict.cpp
@@ -5,7 +5,7 @@
 int main() {
     double          theta0 = 0.0, theta1 = 0.0, millage = 0.0;
     std::string     line;
-    std::ifstream   in("../../coefficient.txt");
+    std::ifstream   in("../../resources/coefficient.txt");
     if (in.is_open()) {
         std::getline(in,line);
         theta0 = std::stod(line);
