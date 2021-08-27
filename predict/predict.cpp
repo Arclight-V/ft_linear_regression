a1a0fb781253575781a031771a73371e3da71882
Arclight-V <vladimtor@gmail.com>
2021-08-27 16:00:27 +0300

    Implement file opening and coefficient parsing

--- predict/predict.cpp
+++ predict/predict.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+
+int main() {
+    double          theta0 = 0.0, theta1 = 0.0;
+    std::string     line;
+    std::ifstream   in("../../coefficient.txt");
+    if (in.is_open()) {
+        std::getline(in,line);
+        theta0 = std::stod(line);
+        theta1 = std::stod(line.substr(line.find(' '), line.size()));
+        std::cout << theta0 << " " << theta1 << '\n';
+    } else {
+        std::cout << "File opening error\n";
+        return -1;
+    }
+}
+
 //
 // Created by Arclight Natashia on 8/27/21.
 //
