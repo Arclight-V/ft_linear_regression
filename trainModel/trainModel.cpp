47c17826d79fea994593ea014fd54a5c4696ebbd
OUT-Kalanchin-VV <VVKalanchin@sberbank@mail.com>
2021-12-06 18:58:01 +0300

    normalize indicators

--- trainModel/trainModel.cpp
+++ trainModel/trainModel.cpp
@@ -10,6 +10,24 @@ int main() {
     std::vector X(parserCsv.getColumns(column_name_km));
     std::vector y(parserCsv.getColumns(column_name_prise));
 
+    std::sort(X.begin(), X.end());
+    std::sort(y.begin(), y.end());
+
+    /*
+     * нормализуем показатели
+     * нужно чтобы корректно выставить коэффициент и крутить линии регрессии относительно оси асцисс (чтобы не уползать в бесконечность)
+     * https://neuronus.com/theory/nn/925-sposoby-normalizatsii-peremennykh.html
+     */
+
+    auto X_min = X[0];
+    auto X_max = X[X.size() - 1];
+
+    std::vector<double> X_norm(X.size());
+
+    for (size_t i = 0; i < X.size(); ++i) {
+        X_norm[i] = (X[i] - X_min) / (X_max - X_min);
+    }
+
     return 0;
 }
 
